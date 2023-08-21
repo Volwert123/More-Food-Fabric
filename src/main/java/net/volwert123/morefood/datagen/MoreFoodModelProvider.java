a4d4bf49ad1d9932defb2391fd1ae0cb76a3f1a6
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-08-21 19:59:29 +0200

    Changed MoreFoodModelProvider.java

--- src/main/java/net/volwert123/morefood/datagen/MoreFoodModelProvider.java
+++ src/main/java/net/volwert123/morefood/datagen/MoreFoodModelProvider.java
@@ -26,6 +26,7 @@ public class MoreFoodModelProvider extends FabricModelProvider {
         itemModelGenerator.register(MoreFoodItems.CARROT_SOUP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.CARROT_BREAD, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.CARROT_PIE, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_APPLE, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_APPLE, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.EMERALD_APPLE, Models.GENERATED);
@@ -34,12 +35,14 @@ public class MoreFoodModelProvider extends FabricModelProvider {
         itemModelGenerator.register(MoreFoodItems.APPLE_SOUP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.APPLE_BREAD, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.APPLE_PIE, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_KELP, Models.GENERATED);
-        itemModelGenerator.register(MoreFoodItems.Gold_KELP, Models.GENERATED);
-        itemModelGenerator.register(MoreFoodItems.Diamond_KELP, Models.GENERATED);
-        itemModelGenerator.register(MoreFoodItems.Emerald_KELP, Models.GENERATED);
+        itemModelGenerator.register(MoreFoodItems.GOLD_KELP, Models.GENERATED);
+        itemModelGenerator.register(MoreFoodItems.DIAMOND_KELP, Models.GENERATED);
+        itemModelGenerator.register(MoreFoodItems.EMERALD_KELP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.KELP_PIECES, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.KELP_SOUP, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_POTATO, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_POTATO, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_POTATO, Models.GENERATED);
@@ -47,36 +50,46 @@ public class MoreFoodModelProvider extends FabricModelProvider {
         itemModelGenerator.register(MoreFoodItems.POTATO_PIECES, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.POTATO_SOUP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.POTATO_BREAD, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.COOKED_PHANTOM, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.PHANTOM_PIECES, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.PHANTOM_SOUP, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.PUFFERFISH_PIECES, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.PUFFERFISH_SOUP, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.PUMPKING_SOUP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.PUMPKING_BREAD, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.COOKED_BAMBOO, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.BAMBOO_PIECES, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.BAMBOO_SOUP, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_COOKED_RABBIT, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_COOKED_RABBIT, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_COOKED_RABBIT, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.EMERALD_COOKED_RABBIT, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_MELON_SLICE, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_MELON_SLICE, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_MELON_SLICE, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.EMERALD_MELON_SLICE, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_COOKED_CHICKEN, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_COOKED_CHICKEN, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_COOKED_CHICKEN, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.EMERALD_COOKED_CHICKEN, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_COOKED_MUTTON, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_COOKED_MUTTON, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_COOKED_MUTTON, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.EMERALD_COOKED_MUTTON, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_COOKED_PORKCHOP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_COOKED_PORKCHOP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_COOKED_PORKCHOP, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.EMERALD_COOKED_PORKCHOP, Models.GENERATED);
+
         itemModelGenerator.register(MoreFoodItems.IRON_COOKED_BEEF, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.GOLD_COOKED_BEEF, Models.GENERATED);
         itemModelGenerator.register(MoreFoodItems.DIAMOND_COOKED_BEEF, Models.GENERATED);
