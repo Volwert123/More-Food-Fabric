27c05fe30b65fa449a0b4b3ec63511a02b493ad8
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-08-21 17:32:33 +0200

    Added PufferfishSoupItem.java and SoupItem.java

---
+++ src/main/java/net/volwert123/morefood/item/custom/SoupItem.java
@@ -0,0 +1,38 @@
+package net.volwert123.morefood.item.custom;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
+
+public class SoupItem extends Item {
+    public SoupItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        PlayerEntity player = user instanceof PlayerEntity ? (PlayerEntity) user : null;
+        if (player instanceof ServerPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger((ServerPlayerEntity) player, stack);
+        }
+        if (player != null){
+            player.incrementStat(Stats.USED.getOrCreateStat(this));
+            player.getHungerManager().add(6, 3f);
+        }
+        if (player == null || !player.getAbilities().creativeMode){
+            if (stack.isEmpty()){
+                return new ItemStack(Items.BOWL);
+            }
+            if (player != null){
+                player.getInventory().insertStack(new ItemStack(Items.BOWL));
+            }
+        }
+        return super.finishUsing(stack, world, user);
+    }
+}
