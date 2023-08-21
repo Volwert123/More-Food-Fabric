87efc8df6905539120bd415eb3c62c8872d438aa
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-08-21 19:54:07 +0200

    Changed MoreFoodItemGroups.java

--- src/main/java/net/volwert123/morefood/item/MoreFoodItemGroups.java
+++ src/main/java/net/volwert123/morefood/item/MoreFoodItemGroups.java
@@ -10,8 +10,7 @@ import net.minecraft.util.Identifier;
 import net.volwert123.morefood.MoreFood;
 
 public class MoreFoodItemGroups {
-    public static ItemGroup MORE_FOOD_TAB = Registry.register(Registries.ITEM_GROUP, new Identifier(MoreFood.MOD_ID, "morefood"),
-            FabricItemGroup.builder()
+    public static ItemGroup MORE_FOOD_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(MoreFood.MOD_ID, "morefood"), FabricItemGroup.builder()
                     .displayName(Text.literal("More Food"))
                     .icon(() -> new ItemStack(MoreFoodItems.COOKED_CARROT))
                     .entries(((displayContext, entries) -> {
@@ -23,6 +22,7 @@ public class MoreFoodItemGroups {
                         entries.add(MoreFoodItems.CARROT_SOUP);
                         entries.add(MoreFoodItems.CARROT_BREAD);
                         entries.add(MoreFoodItems.CARROT_PIE);
+
                         entries.add(MoreFoodItems.IRON_APPLE);
                         entries.add(MoreFoodItems.DIAMOND_APPLE);
                         entries.add(MoreFoodItems.EMERALD_APPLE);
@@ -31,12 +31,14 @@ public class MoreFoodItemGroups {
                         entries.add(MoreFoodItems.APPLE_SOUP);
                         entries.add(MoreFoodItems.APPLE_BREAD);
                         entries.add(MoreFoodItems.APPLE_PIE);
+
                         entries.add(MoreFoodItems.IRON_KELP);
-                        entries.add(MoreFoodItems.Gold_KELP);
-                        entries.add(MoreFoodItems.Diamond_KELP);
-                        entries.add(MoreFoodItems.Emerald_KELP);
+                        entries.add(MoreFoodItems.GOLD_KELP);
+                        entries.add(MoreFoodItems.DIAMOND_KELP);
+                        entries.add(MoreFoodItems.EMERALD_KELP);
                         entries.add(MoreFoodItems.KELP_PIECES);
                         entries.add(MoreFoodItems.KELP_SOUP);
+
                         entries.add(MoreFoodItems.IRON_POTATO);
                         entries.add(MoreFoodItems.GOLD_POTATO);
                         entries.add(MoreFoodItems.DIAMOND_POTATO);
@@ -44,36 +46,46 @@ public class MoreFoodItemGroups {
                         entries.add(MoreFoodItems.POTATO_PIECES);
                         entries.add(MoreFoodItems.POTATO_SOUP);
                         entries.add(MoreFoodItems.POTATO_BREAD);
+
                         entries.add(MoreFoodItems.COOKED_PHANTOM);
                         entries.add(MoreFoodItems.PHANTOM_PIECES);
                         entries.add(MoreFoodItems.PHANTOM_SOUP);
+
                         entries.add(MoreFoodItems.PUFFERFISH_PIECES);
                         entries.add(MoreFoodItems.PUFFERFISH_SOUP);
+
                         entries.add(MoreFoodItems.PUMPKING_SOUP);
                         entries.add(MoreFoodItems.PUMPKING_BREAD);
+
                         entries.add(MoreFoodItems.COOKED_BAMBOO);
                         entries.add(MoreFoodItems.BAMBOO_PIECES);
                         entries.add(MoreFoodItems.BAMBOO_SOUP);
+
                         entries.add(MoreFoodItems.IRON_COOKED_RABBIT);
                         entries.add(MoreFoodItems.GOLD_COOKED_RABBIT);
                         entries.add(MoreFoodItems.DIAMOND_COOKED_RABBIT);
                         entries.add(MoreFoodItems.EMERALD_COOKED_RABBIT);
+
                         entries.add(MoreFoodItems.IRON_MELON_SLICE);
                         entries.add(MoreFoodItems.GOLD_MELON_SLICE);
                         entries.add(MoreFoodItems.DIAMOND_MELON_SLICE);
                         entries.add(MoreFoodItems.EMERALD_MELON_SLICE);
+
                         entries.add(MoreFoodItems.IRON_COOKED_CHICKEN);
                         entries.add(MoreFoodItems.GOLD_COOKED_CHICKEN);
                         entries.add(MoreFoodItems.DIAMOND_COOKED_CHICKEN);
                         entries.add(MoreFoodItems.EMERALD_COOKED_CHICKEN);
+
                         entries.add(MoreFoodItems.IRON_COOKED_MUTTON);
                         entries.add(MoreFoodItems.GOLD_COOKED_MUTTON);
                         entries.add(MoreFoodItems.DIAMOND_COOKED_MUTTON);
                         entries.add(MoreFoodItems.EMERALD_COOKED_MUTTON);
+
                         entries.add(MoreFoodItems.IRON_COOKED_PORKCHOP);
                         entries.add(MoreFoodItems.GOLD_COOKED_PORKCHOP);
                         entries.add(MoreFoodItems.DIAMOND_COOKED_PORKCHOP);
                         entries.add(MoreFoodItems.EMERALD_COOKED_PORKCHOP);
+
                         entries.add(MoreFoodItems.IRON_COOKED_BEEF);
                         entries.add(MoreFoodItems.GOLD_COOKED_BEEF);
                         entries.add(MoreFoodItems.DIAMOND_COOKED_BEEF);
@@ -81,6 +93,6 @@ public class MoreFoodItemGroups {
                     })).build());
 
     public static void registerItemGroups() {
-        MoreFood.LOGGER.info("Registering ModItemGroups for " + MoreFood.MOD_ID);
+        MoreFood.LOGGER.info("Registering ItemGroups for " + MoreFood.MOD_ID);
     }
 }
