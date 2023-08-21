d7d0952278377b333e63eafe25b4115a8481f29f
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-08-21 18:21:52 +0200

    Added IronFoodItem.java, GoldenFoodItem.java, DiamondFoodItem.java and EmeraldFoodItem.java

---
+++ src/main/java/net/volwert123/morefood/item/custom/EmeraldFoodItem.java
@@ -0,0 +1,32 @@
+package net.volwert123.morefood.item.custom;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
+
+public class EmeraldFoodItem extends Item {
+    public EmeraldFoodItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        PlayerEntity player = user instanceof PlayerEntity ? (PlayerEntity) user : null;
+        if (player instanceof ServerPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger((ServerPlayerEntity) player, stack);
+        }
+        if (world.isClient) user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1200));
+        if (world.isClient) user.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1200, 3));
+        if (player != null) {
+            player.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+        return super.finishUsing(stack, world, user);
+    }
+}
