f8cc323858f5cd11ceb6d7e4fa472b2f1c7806a5
Volwert123 <Volwert123@gmail.com>
2023-09-03 16:44:01 +0200

    Fixed bugs

--- src/main/java/net/volwert123/morefood/item/custom/RiceBowlItem.java
+++ src/main/java/net/volwert123/morefood/item/custom/RiceBowlItem.java
@@ -1,9 +1,37 @@
 package net.volwert123.morefood.item.custom;
 
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
 
 public class RiceBowlItem extends Item {
     public RiceBowlItem(Settings settings) {
         super(settings.maxCount(1));
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        PlayerEntity player = user instanceof PlayerEntity ? (PlayerEntity) user : null;
+        if (player instanceof ServerPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger((ServerPlayerEntity) player, stack);
+        }
+        if (player != null){
+            player.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+        if (player == null || !player.getAbilities().creativeMode){
+            if (stack.isEmpty()){
+                return new ItemStack(Items.BOWL);
+            }
+            if (player != null){
+                player.getInventory().insertStack(new ItemStack(Items.BOWL));
+            }
+        }
+        return super.finishUsing(stack, world, user);
     }
 }
